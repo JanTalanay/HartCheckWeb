8855a761b0c23f9cf03b655978541c7f8c2c5039
JustinAngCSB <106389722+JustinAngCSB@users.noreply.github.com>
2023-10-09 22:57:53 +0800

    Added project files

---
+++ HartCheck-Admin/Interfaces/IPatientRepository.cs
@@ -0,0 +1,16 @@
+﻿using HartCheck_Admin.Models;
+
+namespace HartCheck_Admin.Interfaces
+{
+    public interface IPatientRepository
+    {
+        Task<IEnumerable<Patient>> GetAll();
+        Task<Patient> GetByIdAsync(int id);
+        Task<Patient> GetByIdAsyncNoTracking(int id);
+
+        bool Add(Patient patient);
+        bool Update(Patient patient);  
+        bool Delete(Patient patient);
+        bool Save();
+    }
+}
