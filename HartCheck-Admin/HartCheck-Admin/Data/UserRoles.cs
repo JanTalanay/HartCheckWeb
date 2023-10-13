c21a6a87686c54d93252ff6abad72fc744cf6f9e
JustinAngCSB <106389722+JustinAngCSB@users.noreply.github.com>
2023-10-13 18:34:05 +0800

    Initial Bug Report Features

---
+++ HartCheck-Admin/HartCheck-Admin/Data/UserRoles.cs
@@ -0,0 +1,8 @@
+﻿namespace HartCheck_Admin.Data
+{
+    public class UserRoles
+    {
+        public const string Admin = "admin";
+        public const string User = "user";
+    }
+}
---
+++ HartCheck-Admin/HartCheck-Admin/ViewModels/EditEducationalResourceViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HartCheck_Admin.ViewModels
+{
+    public class EditEducationalResourceViewModel
+    {
+        public int resourceID{ get; set; }
+        public string text { get; set; }
+        public string link { get; set; }
+    }
+}
