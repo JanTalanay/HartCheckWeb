cecf00b1cf213521d25f91b21e3944fe46995f05
Jems <james@DESKTOP-NBIUVHB>
2023-11-07 12:23:48 +0800

    added datacontext

--- HartCheck_Doctor_test/Program.cs
+++ HartCheck_Doctor_test/Program.cs
@@ -1,7 +1,15 @@
+using HartCheck_Doctor_test.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<datacontext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 
 var app = builder.Build();
 
